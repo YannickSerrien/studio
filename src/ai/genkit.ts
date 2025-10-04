a2a3f4e2f4460dd0acd8fe3323bf4e9a18d359f4
Yannick Serrien <yannick.serrien110@gmail.com>
2025-10-04 17:51:10 +0000

    Why do we need the two .env files? Also make sure that the gemini model

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,9 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import 'dotenv/config';
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
-  model: 'googleai/gemini-2.5-flash',
+  model: 'googleai/gemini-pro',
 });
